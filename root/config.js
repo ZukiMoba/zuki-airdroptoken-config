8e8d115eade690da02e82645d33546a4b7dacaac
dilysbi <ngohung2395@gmail.com>
2022-01-10 20:59:04 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,4 +1,4 @@
 {
-  "end_waiting_claim": "Jan 10 2022 20:57:38 GMT+0700",
-  "end_claim":         "Jan 10 2022 21:00:00 GMT+0700"
+  "end_waiting_claim": "Jan 10 2022 21:00:00 GMT+0700",
+  "end_claim":         "Jan 10 2022 21:02:00 GMT+0700"
 }
