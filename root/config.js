ab6b184e4efdf9983ba773490827759ae090f445
Hao2505 <73099324+Hao2505@users.noreply.github.com>
2022-01-10 11:07:19 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,9 +1,3 @@
 {
-  "isCheckWhiteList": 0,
-  "start":             "Dec 20 2021 01:01:00 GMT+0700",
-  "end_round_1":       "Dec 20 2021 01:02:00 GMT+0700",
-  "end_round_2":       "Dec 21 2021 01:03:00 GMT+0700",
-  "end_waiting_claim": "Dec 21 2021 01:04:00 GMT+0700",
-  "count_round_1": "400",
-  "count_round_2": "1200"
+  "end_claim":       "Jan 011 2022 13:04:21 GMT+0700",
 }
