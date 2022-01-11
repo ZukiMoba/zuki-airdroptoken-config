d992866c5546bd282f7f2cd6f8d84fcde6b36080
Hao2505 <73099324+Hao2505@users.noreply.github.com>
2022-01-11 09:28:26 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,4 +1,4 @@
 {
-  "end_waiting_claim": "Jan 10 2022 21:00:00 GMT+0700",
-  "end_claim":         "Jan 10 2022 22:02:00 GMT+0700"
+  "end_waiting_claim": "Jan 11 2022 21:00:00 GMT+0700",
+  "end_claim":         "Jan 12 2022 22:02:00 GMT+0700"
 }
