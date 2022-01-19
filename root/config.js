79f4db33c82bee0cc46f514a75df6ec3cf8e6559
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2022-01-19 12:17:44 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,4 +1,4 @@
 {
-  "end_waiting_claim": "Jan 18 2022 10:00:00 GMT+0700",
-  "end_claim":         "Jan 19 2022 10:00:00 GMT+0700"
+  "end_waiting_claim": "Jan 19 2022 12:20:00 GMT+0700",
+  "end_claim":         "Jan 19 2022 12:25:00 GMT+0700"
 }
